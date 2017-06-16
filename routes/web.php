f114c02a208c59fe2658aa317fdea2f037a06830
symbiancross <atomicsecure@gmail.com>
2017-06-16 22:59:59 +0700

    middleware

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 
 Route::get('/', function () {
     return view('auth/login');
-});
+})->middleware('guest');
 
 Route::get('/role', 'RoleController@index');
 Route::post('/role', 'RoleController@store');
